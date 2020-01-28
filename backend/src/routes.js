e8ded42069ab284ca6370a925ee0156b0e24c65a
Michelon Souza <michelon.souza@hotmail.com>
2020-01-27 23:59:08 -0300

    implements auth middlware

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
