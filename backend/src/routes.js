5d6f68be7053b48f4d1251f1176c541ff5a77d55
Michelon Souza <michelon.souza@hotmail.com>
2020-01-27 23:43:06 -0300

    implements jwt athentication

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
